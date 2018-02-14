0538b0718f190d65f50cbb016dd96f11248c8f56
RyanMcgrane <c16419862@mydit.ie>
2018-02-14 16:32:55 +0000

    Tune class Created With accessors

---
+++ ie/dit/Tune.java
@@ -0,0 +1,35 @@
+public class Tune
+{
+	private int x;
+	private String title;
+	private String altTitle;
+	private String notation;
+	
+	
+	public getx()
+	{
+		return x;
+	}
+	
+	public getTitle()
+	{
+		return title;
+	}
+	
+	public getAltTitle()
+	{
+		return altTitle;
+	}
+	
+	public getNotation()
+	{
+		return notation;
+	}
+
+	public String toString()
+	{
+		return "'" + x + ", " + title + ", " + altTitle + "'";
+	}
+
+
+}
